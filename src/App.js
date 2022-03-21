057ecb9de55dbe6d72bf566b298d1c3d0c09deab
Kirill Seredin <k.seredin@rambler.ru>
2022-03-21 15:51:44 +0200

    Inplement mock links and made footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import { Layout, Space, Typography } from 'antd'
-import React from 'react'
-import {Switch, Route, Link} from 'react-router-dom'
-import {Navbar} from './components'
-import './App.css'
-const App = () =>
-{
-
+import { Layout, Space, Typography } from "antd";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
+const App = () => {
   return (
     <div className="app">
-<div className="navbar">
-  <Navbar/>
-</div>  
-<div className="main">
-
-</div>
-<div className="footer">
-
-</div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Crypto <br />
+            All rights reserverd
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/">Exchanges</Link>
+            <Link to="/">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
