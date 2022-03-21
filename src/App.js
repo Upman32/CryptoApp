8a24f1d7a2c45b937315fb9796fe6ed8d80e9c05
Kirill Seredin <k.seredin@rambler.ru>
2022-03-21 15:20:15 +0200

    Embed App, deleted start-files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Layout, Space, Typography } from 'antd'
+import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
+import {Navbar} from './components'
+import './App.css'
+const App = () =>
+{
+
+  return (
+    <div className="app">
+<div className="navbar">
+  <Navbar/>
+</div>  
+<div className="main">
+
+</div>
+<div className="footer">
+
+</div>
+    </div>
+  )
+}
+export default App
